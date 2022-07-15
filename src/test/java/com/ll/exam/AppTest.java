3e4fb8d661f44d41ded66aaa1fa4ec2ccd81240b
ahah525 <ahah525@naver.com>
2022-07-15 14:14:23 +0900

    종료 기능 구현 및 테스트

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -6,7 +6,6 @@ import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
@@ -27,10 +26,11 @@ public class AppTest {
 
     @Test
     public void 출력을_모니터가_아닌_문자열로_반환하기() {
-        // 모니터에 출력할 문자열이 모두 output에 담김)
+        // 모니터에 출력할 문자열이 모두 output에 담김
         ByteArrayOutputStream output = TestUtil.setOutToByteArray();
         System.out.print("출력");
         String s = output.toString();
+        // 출력 방향을 표준 출력으로 변경
         TestUtil.clearSetOutToByteArray(output);
         // 출력된 문자열이 "출력"이 맞는지 검증
         assertThat(s).isEqualTo("출력");
@@ -44,5 +44,19 @@ public class AppTest {
         String body = Util.file.readFromFile("test_data/1.txt");
         // 파일에 저장한 문자열이 실제 파일에서 읽어온 문자열과 동일한지 검증
         assertThat(body).isEqualTo("데이터");
+    }
+
+    @Test
+    public void 프로그램이_시작하면_시작문구_출력() {
+        Scanner sc = TestUtil.genScanner("종료");
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+
+        new App(sc).run();
+
+        String s = output.toString();
+        TestUtil.clearSetOutToByteArray(output);
+        // 프로그램 시작이되었을 때 문구가 잘 출력되는지 검증
+        assertTrue(s.contains("== 명언 SSG =="));
+        assertTrue(s.contains("명령)"));
     }
 }
