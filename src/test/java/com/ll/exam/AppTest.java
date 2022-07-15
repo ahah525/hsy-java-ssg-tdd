07205f106c010a7257a905c8d91401743c981f26
ahah525 <ahah525@naver.com>
2022-07-15 13:52:36 +0900

    파일 입출력을 위한 Util 클래스 구현 및 파일 입출력 테스트

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -35,4 +35,14 @@ public class AppTest {
         // 출력된 문자열이 "출력"이 맞는지 검증
         assertThat(s).isEqualTo("출력");
     }
+
+    @Test
+    public void 문자열_파일에_저장하기() {
+        // 디렉토리 생성 후, 해당 path 파일에 body 쓰기
+        Util.file.mkdir("test_data");
+        Util.file.saveToFile("test_data/1.txt", "데이터");
+        String body = Util.file.readFromFile("test_data/1.txt");
+        // 파일에 저장한 문자열이 실제 파일에서 읽어온 문자열과 동일한지 검증
+        assertThat(body).isEqualTo("데이터");
+    }
 }
